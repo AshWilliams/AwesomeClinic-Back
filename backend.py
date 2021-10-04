856f17073a48a0ed027239f49d09aa5a629cf146
Robert Rozas <rorozasn@microsoft.com>
2021-10-04 11:02:47 -0300

    backend mock 80%...dockerfile ok...todo - test

--- backend.py
+++ backend.py
@@ -4,15 +4,29 @@ app = Flask(__name__)
 
 @app.route('/profile/')
 def profile():
-    return jsonify({'name':'Jimit',
-                    'address':'India'})
+    return jsonify({'name':'Robert Rozas Navarro',
+                    'phone':'+56 9 5811 9900',
+                    'address':'Middle of Nowhere, Chile',
+                    'member_since':'July 2012'})
 
 @app.route('/appointments/')
 def appointmentList():
-    return jsonify([{'name':'Jimit',
-                    'address':'India'},
-                    {'name':'Jhon',
-                    'address':'Chile'}])
+    return jsonify([{'date':'10/10/2021',
+                     'doctor':'Sergio Palma',
+                     'specialty':'Gastroenterólogo',
+                     'branch_address':'Las Condes'},
+                    {'date':'23/10/2021',
+                     'doctor':'Juan Soto',
+                     'specialty':'Medicina General',
+                     'branch_address':'Nuñoa'},
+                    {'date':'10/11/2021',
+                     'doctor':'Emilia Albuquerque',
+                     'specialty':'Oftalmologo',
+                     'branch_address':'Tobalaba'},
+                    {'date':'28/11/2021',
+                     'doctor':'Pedro Chandía',
+                     'specialty':'Dentista',
+                     'branch_address':'Las Condes'}])
 
 #pip install flask
 #export FLASK_APP=backend.py
